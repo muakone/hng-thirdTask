47ed7041d64454ae8477ae94627540b2c76c5c5f
Muhiz Akanni <muak1ayomide@gmail.com>
2022-11-15 16:43:57 +0100

    feat: added the navbar and footer to the app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom"
 import PlaceToStay from './pages/PlaceToStay';
 import Home from './pages/Home';
 import './App.css';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 
 function App() {
   return (
     <div>
+    <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/place-to-stay' element={<PlaceToStay />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
